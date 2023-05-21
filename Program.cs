1ec5f7f4d6f1132eb470650a9f2948ece3575964
jaison080 <jaisondennis080@gmail.com>
2023-05-21 12:13:07 +0530

    Minor Bug Fix

--- Program.cs
+++ Program.cs
@@ -18,13 +18,6 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
